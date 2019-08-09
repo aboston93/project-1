d698326b8f2dbc0d0b908ee90005919a6cc3ba4d
Anthony Boston <anthonybostonCB5@MacBook-Air-96.local>
2019-08-09 12:34:06 -0400

    added modalquestions to function

--- app.js
+++ app.js
@@ -2,43 +2,56 @@
 // clickys 
 // local storage
 
+
 // global variables that will be used as the questions 
-let questions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-let answers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-let p = Math.floor((Math.random() * 15) + 1);
-
-let i = Number(p)
+let questions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
+let answers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
+let yard20
+let yard40
+let yard60
+let yard80
+let TD
+// let p = Math.floor((Math.random() * 15) + 1);
+// let i = Number(p)
 // Modal question  function appears 
-// function MakeModalApper() {
-$(".box").click(function () {
-    $('#question-modal').css({
-        "display": "block",
-    })
-    
+function MakeModalAppear(modalQuestions) {
+    $(".box").click(function () {
+        $('#question-modal').css({
+            "display": "block",
+        })
+        console.log('im inside make modal appear')
 
-    $('.box').html(questions[i])
-    $('#question-title').html(questions[i])
-    $()
-    console.log(i, questions)
-})
-// }
-// function MakeQModalApper(){
-$("#Question-button").click(function () {
-    $('#answer-modal').css({
-        "display": "block",
-    })
-    $('#question-modal').css({
-        "display": "none",
+        $('.box').html(modalQuestions)
+        console.log(modalQuestions)
+
+        $('#question-title').html(modalQuestions)
+        MakeAModalAppear()
     })
 
+}
 
-    $('.box').html(questions[i])
-    $('#answer-title').html(questions[i])
-    
-    console.log(i, questions)
-})
-// }
 
+function MakeAModalAppear(modalAnswers) {
+    $("#answer-button").click(function () {
+        $('#answer-modal').css({
+            "display": "block",
+        })
+        $('#question-modal').css({
+            "display": "none",
+        })
+
+
+        $('.box').html(modalAnswers)
+        $('#answer-title').html(modalAnswers)
+
+        console.log(modalAnswers)
+    })
+}
+for (let i = 0; i < 15; i++) {
+    MakeModalAppear(questions[i])
+    //
+    // MakeAModalAppear(answers[i])
+}
 
 // make the divs be populated with the answers
 
