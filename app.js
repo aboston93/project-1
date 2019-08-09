c6e544fe8aa0ebd103c1bfe68fa343f40c20302e
Anthony Boston <anthonybostonCB5@MacBook-Air-96.local>
2019-08-09 11:29:10 -0400

    put some of my  fixed function storage  and figured out how to call them

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+// session Storage
+// clickys 
+// local storage
 
+// global variables that will be used as the questions 
 let questions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 let answers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 let p = Math.floor((Math.random() * 15) + 1);
 
 let i = Number(p)
-// Modal appear function 
-
+// Modal question  function appears 
+// function MakeModalApper() {
 $(".box").click(function () {
     $('#question-modal').css({
         "display": "block",
@@ -17,8 +21,8 @@ $(".box").click(function () {
     $()
     console.log(i, questions)
 })
-
-
+// }
+// function MakeQModalApper(){
 $("#Question-button").click(function () {
     $('#answer-modal').css({
         "display": "block",
@@ -26,14 +30,32 @@ $("#Question-button").click(function () {
     $('#question-modal').css({
         "display": "none",
     })
-    
+
 
     $('.box').html(questions[i])
     $('#answer-title').html(questions[i])
-    $()
+    
     console.log(i, questions)
 })
+// }
 
+
+// make the divs be populated with the answers
+
+
+// compare question to answers
+
+// Point divs need to dissappear in after being answerd
+
+
+// Total points need to be confirmed  
+
+// total score needs to be confirmed 
+
+// once the total score meets 3 the game needs to notify player 
+
+
+// 
 
 
 //       $( "button" ).click(function() {
