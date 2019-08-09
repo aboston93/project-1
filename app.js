a2dad3d90b95caa0da9b990c0ba6ddcbcce43115
Anthony Boston <anthonybostonCB5@MacBook-Air-96.local>
2019-08-09 09:52:02 -0400

    globally scoped my variables after having trouble with calling them in my functions

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 
+let questions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+let answers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+let p = Math.floor((Math.random() * 15) + 1);
 
+let i = Number(p)
 // Modal appear function 
 
-    $( ".box" ).click(function() {
-       $('#question-modal').css({
-        "display": "block",})
-let questions = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-let answers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-let p=Math.floor((Math.random() * 15) + 1);
-
-let i =Number(p)
-
-$('.box').html(questions[i])
- $('#question-title').html(questions[i])
-     console.log(i,questions)
+$(".box").click(function () {
+    $('#question-modal').css({
+        "display": "block",
     })
-      
-      
-      
-      
-      
-      
+    
+
+    $('.box').html(questions[i])
+    $('#question-title').html(questions[i])
+    $()
+    console.log(i, questions)
+})
+
+
+$("#Question-button").click(function () {
+    $('#answer-modal').css({
+        "display": "block",
+    })
+    $('#question-modal').css({
+        "display": "none",
+    })
+    
+
+    $('.box').html(questions[i])
+    $('#answer-title').html(questions[i])
+    $()
+    console.log(i, questions)
+})
+
+
+
 //       $( "button" ).click(function() {
 //         if ( p ) {
 //           p.appendTo( "body" );
