3fb19652c023d15ab0cae4eb3d4cb4bbb4a25c3d
Anthony Boston <anthonybostonCB5@MacBook-Air-96.local>
2019-08-12 04:55:33 -0400

    adding questions to the arrays

--- app.js
+++ app.js
@@ -6,15 +6,14 @@ let x
 // global variables that will be used as the questions 
 
 let yard20 = ['how many points are a tpuchdown worth', 'what color is a football', 'how ,any yards are there in a football field', 'how many teams are in the nfl', 'how many players can be on a football field']
-let yard60 = ['What NFL team is owned by two familys related by marriage ', 'What round did the greatest quarterback of all time get drafted', 'What college did terrell owens go to', 'how many superbowls have Tom Brady won ', 'What is the name of Quarterback run play right up the middle', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
-let yard80 = ['How many of the five Dallas teams Tom Landry took to Super Bowls were victorious', 'What NFL team introduced the no-huddle offense during the 1980s', 'What quarterback got stuck with the given names Yelberton Abraham', 'What Baltimore Colts great led the NFL in passing in the 1960s, with 26548 yards', 'What Pittsburgh Steelers great was the first wide receiver to be named super Bowl MVP', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
+let yard60 = ['What NFL team is owned by two familys related by marriage ', 'What round did the greatest quarterback of all time get drafted', 'What college did terrell owens go to', 'how many superbowls have Tom Brady won ', 'What is the name of Quarterback run play right up the middle']
+let yard80 = ['How many of the five Dallas teams Tom Landry took to Super Bowls were victorious', 'What NFL team introduced the no-huddle offense during the 1980s', 'What quarterback got stuck with the given names Yelberton Abraham', 'What Baltimore Colts great led the NFL in passing in the 1960s, with 26548 yards', 'What Pittsburgh Steelers great was the first wide receiver to be named super Bowl MVP']
 let yard100 = ['What Native American language was Super Bowl30 the first to be broadcast in?', 'What Cowboys 99-yard run from scrimmage put him in the NFL football record book in 1983?',
- 'What team hired the NFLs first professional cheerleading squad, in 1972?', 'Who was the first athlete to rap at a Pro Bowl musical gala', 'What famous trick play did the phidelphia eagles run in the superbowl win', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
-let answer20 = ['seven', 'brown', 'one hundred', 'thirty two', 'eleven', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
-let answer40 = ['eleven', 'one', 'megatron', 'three', 'falcons', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
-let answer60 = ['Megatgiants and steelers ', '199', 'University of Tennessee at Chattanooga', 'six', 'QB Sneak', '', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
-let answer80 = ['two', 'Cincinnati Bengals ', 'Y.A. Tittle.', 'Johnny Unitas', 'Lynn Swan', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
-let answer100 = ['Navajo', 'Tony Dorsetts', ' Dallas Cowboys ', 'Deion Sanders', 'philly special', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
+    'What team hired the NFLs first professional cheerleading squad, in 1972?', 'Who was the first athlete to rap at a Pro Bowl musical gala', 'What famous trick play did the phidelphia eagles run in the superbowl win']
+let answer40 = ['eleven', 'one', 'megatron', 'three', 'falcons']
+let answer60 = ['Megatgiants and steelers ', '199', 'University of Tennessee at Chattanooga', 'six', 'QB Sneak']
+let answer80 = ['two', 'Cincinnati Bengals ', 'Y.A. Tittle.', 'Johnny Unitas', 'Lynn Swan']
+let answer100 = ['Navajo', 'Tony Dorsetts', ' Dallas Cowboys ', 'Deion Sanders', 'philly special']
 // let p = Math.floor((Math.random() * 15) );
 // let i = Number(p)
 
@@ -23,15 +22,15 @@ let answer100 = ['Navajo', 'Tony Dorsetts', ' Dallas Cowboys ', 'Deion Sanders',
 // RandoArr = [generateQuestion()]
 // return RandoArr;
 // }
-let wrongA=0
+let wrongA = 0
 // Modal question  function appears  
 //comparing html of the div upon its intial click and the  depending on the value posting a question.
 function MakeModalAppear() {
     $(document).ready(function () {
         $(".box").click(function () {
-        x= $(this).html()
-            let p = Math.floor((Math.random() * 5) );
-             let i = Number(p)
+            x = $(this).html()
+            let p = Math.floor((Math.random() * 5));
+            let i = Number(p)
             $(this).append('<img src="Untitled.png">')
             console.log(x)
             if (x == 20) {
@@ -65,12 +64,12 @@ function MakeModalAppear() {
 
             }
 
-            if(wrongA>=3){
+            if (wrongA >= 3) {
                 window.alert("You  lost the SEI SUPERBOWL")
                 console.log("why cant i lose!!!")
             }
-            
-            
+
+
 
             $('#question-modal').css({
                 "display": "block",
@@ -82,13 +81,13 @@ function MakeModalAppear() {
 
             $('#question-title').html()
             MakeAModalAppear()
-           
+
         })
     }
     )
 }
 
-
+//makes the answer modal appear  as well as make the question modal dissappeard
 function MakeAModalAppear() {
     $("#answer-button").click(function () {
         $('#answer-modal').css({
@@ -101,7 +100,7 @@ function MakeAModalAppear() {
 
         $('.box').html()
         $('#answer-title').html()
-      
+
         console.log()
     })
 }
@@ -109,31 +108,31 @@ function MakeAModalAppear() {
 // user to check the value of 
 // the input anc check the answer
 function submitAnswer() {
-    
+
     $("#confirm-button").click(function () {
         l = $('#user').val()
         j = $('#question-title').html()
         f = $('#answer-title').html()
         e = $('.scoreb').html()
-        let oldscore=0
-         p = 0 + Number(x)
-         totalScore=Number(x)+Number(e)
+        let oldscore = 0
+        p = 0 + Number(x)
+        totalScore = Number(x) + Number(e)
         console.log(l, j)
 
 
         if (l == n) {
-        
+
             console.log('your right we almost done with the project')
             $('#answer-modal').css({
                 "display": "none",
             })
-            console.log(f, x, e,totalScore,l,n)
-        
-console.log(totalScore)
-           
-           
+            console.log(f, x, e, totalScore, l, n)
+
+            console.log(totalScore)
+
+
             $('#answer-title').html(totalScore)
-           
+
         }
 
 
@@ -141,8 +140,8 @@ console.log(totalScore)
             $('#answer-modal').css({
                 "display": "none",
             })
-wrongA++
-console.log(wrongA)
+            wrongA++
+            console.log(wrongA)
             $('img').remove()
 
         }
@@ -150,11 +149,11 @@ console.log(wrongA)
 
     })
 }
-
+//function used to start the end of the game hold an if statment
 function finishedGame() {
 
     $("#confirm-button").click(function () {
-      
+
         // if (totalScore >= 300) {
         //     window.alert('You have won the SEI SUPERBOWL!!')
         // }
